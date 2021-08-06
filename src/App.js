e833e9991322da91ecb25a211efbe6d01952e8fd
Mabilog <g.angelo.m@gmail.com>
2021-08-06 08:49:53 -0400

    lets just do this

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-// import Card from './components/Card';
+import Card from './components/Card';
 
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
   const [query, setQuery] = useState('');
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState();
   
   const getInfo = (e) =>{
     e.preventDefault();
+    console.log("Query: " + query);
     fetch(`https://api.spoonacular.com/food/ingredients/search?apiKey=${API_KEY}&query=${query}`)
     .then(res => res.json())
     .then(data => {
-      setResults(data)
+      return setResults(data)
     })
-    .then(() => console.log(results))
+    .then(response => console.log(response))
     .catch(err => console.log("Something went wrong! " + err))
+
   }
 
   const setSearch = (e) => {
     e.preventDefault();
     setQuery(e.target.value);
   }
-
+  
   return (
     <div className="App">
       <form >
-        <input type="text" onChange={setSearch} onSubmit={getInfo}/>
-        <button onClick={getInfo}>Search</button>
+        <input type="text" onSubmit={setSearch}/>
+        <button onClick={getInfo} type="submit">Search</button>
       </form>
-      {/* <Card results={results}/> */}
+      <Card results={results}/>
     </div>
   );
 }
