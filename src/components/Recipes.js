2b0418546e8e20839734b79b3cbf33263541de89
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-08-06 16:35:29 -0400

    to throw

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
+// import './scss/recipes.scss'
+const Recipes = () => {
+  // const API_KEY = process.env.REACT_APP_API_KEY;
+  
+  // const getRecipe = (id) => {
+  //   // fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`)
+  //   // .then(res => res.json())
+  //   // .then(data => console.log(data.id))
+  //   // .catch(err => console.log("Something went wrong. no idea what though so go read it yourself " + err ))
+  // }
+  // const handleClick = (id) => {getRecipe(id)}
 
-const Recipes = ({ searchData }) => {
-  console.log(searchData);
+  // console.log(searchData)
+
+  useEffect(() => {
+    fetch(`/fake data/apple.jpg`)
+    .then(res => {return res.blob()})
+    .then(res => console.log(res))
+
+  }, [])
   return (
     <>
     <div className="recipes-card">
       <div className="recipes-body">
-        {searchData.results.map(recipe => {
+        <img src="" id="apple" alt="apple"/>
+        {/* {searchData.results.map(recipe => {
           return(
-            <div>
+            <div key={recipe.id}>
             <h5 className="card-title">{recipe.title}</h5>
             <h6 className="card-subtitle">{recipe.id}</h6>
-            {/* <p className="card-text">Something something too sweet for you bb</p>  */}
-            <img src={recipe.image} alt={recipe.name}/>
+              <img src={recipe.image} alt={recipe.name}/>
+              <button onClick={getRecipe(recipe.id)}>Check Recipe</button>
            </div>
           )
-        })}
+        })} */}
       </div>
     </div>
     </>
