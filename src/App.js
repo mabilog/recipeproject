d7fca677d16c2faa94cbfb1ca86d56c2f02899ea
Mabilog <g.angelo.m@gmail.com>
2021-08-06 19:33:37 -0400

    i dont want to deal with this anymore

--- src/App.js
+++ src/App.js
@@ -4,43 +4,31 @@ import {  useState } from 'react';
 import Recipes from './components/Recipes';
 
 function App() {
-  // const API_KEY = process.env.REACT_APP_API_KEY;
-  // const [query, setQuery] = useState(null);
-  // const [searchData, setSearchData] = useState(null);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [query, setQuery] = useState(null);
+  const [searchData, setSearchData] = useState(null);
   
-  // const handleChange = (e) => {
-  //   setQuery(e.target.value);
-  // }
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  }
 
-  // const getInfo = (e) =>{
-  //   e.preventDefault();
-  //   // console.log("Query: " + query);
-  //   // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${query}`)
-  //   //   .then(res => res.json())
-  //   //   .then(data => setSearchData(data))
-  //   //   .catch(err => console.log("Something went wrong! " + err))
-
-  //   fetch(`/fake data/fake.json`)
-  //     .then(res => res.jsonify())
-  //     .then(data => {
-  //       setSearchData(data)
-  //       console.log(data)})
-  //     .catch(err => console.log("something seems a fucky " + err))
-
-  // }
+  const getInfo = (e) =>{
+    e.preventDefault();
+    console.log("Query: " + query);
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${query}`)
+      .then(res => res.json())
+      .then(data => setSearchData(data))
+      .catch(err => console.log("Something went wrong! " + err))
+  }
 
   
   return (
     <div className="App">
       <form >
-        {/* <input type="text" onChange={handleChange} placeholder="Search"/> 
-                <button onClick={getInfo}>Search</button>
-*/}
-        <input type="text" placeholder="Search"/>
-        <button >Search</button>
-      </form>
-      {/* {searchData &&  <Recipes searchData={searchData}/>} */}
-      <Recipes/>
+        <input type="text" onChange={handleChange} placeholder="Search"/> 
+        <button onClick={getInfo}>Search</button>
+        </form>
+      {searchData &&  <Recipes searchData={searchData}/>}
     </div>
   );
 }
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,39 +1,33 @@
 import React,{ useEffect } from 'react'
 // import './scss/recipes.scss'
-const Recipes = () => {
-  // const API_KEY = process.env.REACT_APP_API_KEY;
+const Recipes = ({searchData}) => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
   
-  // const getRecipe = (id) => {
-  //   // fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`)
-  //   // .then(res => res.json())
-  //   // .then(data => console.log(data.id))
-  //   // .catch(err => console.log("Something went wrong. no idea what though so go read it yourself " + err ))
-  // }
-  // const handleClick = (id) => {getRecipe(id)}
+  const getRecipe = (id) => {
+    fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => console.log(data.id))
+    .catch(err => console.log("Something went wrong. no idea what though so go read it yourself " + err ))
+  }
 
-  // console.log(searchData)
+  console.log(searchData)
 
-  useEffect(() => {
-    fetch(`/fake data/apple.jpg`)
-    .then(res => {return res.blob()})
-    .then(res => console.log(res))
 
-  }, [])
   return (
     <>
     <div className="recipes-card">
       <div className="recipes-body">
         <img src="" id="apple" alt="apple"/>
-        {/* {searchData.results.map(recipe => {
+        {searchData.results.map(recipe => {
           return(
             <div key={recipe.id}>
             <h5 className="card-title">{recipe.title}</h5>
             <h6 className="card-subtitle">{recipe.id}</h6>
               <img src={recipe.image} alt={recipe.name}/>
-              <button onClick={getRecipe(recipe.id)}>Check Recipe</button>
+              <button onClick={() => getRecipe(recipe.id)}>Check Recipe</button>
            </div>
           )
-        })} */}
+        })}
       </div>
     </div>
     </>
